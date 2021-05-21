a8274f1004f05d0b71af8fb2b4e734f4283f7d43
NIKITAZEML <zemlyanukhin-2003@mail.ru>
2021-05-21 15:57:43 +0300

    fdsdssd

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Burger />
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+
+
+</script>
+
+
+<style lang="scss">
+@import "@/assets/styles.scss";
+
+</style>
---
+++ src/components/Burger.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="burger">
+        <div class="lines">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+    methods:{
+        openBurger(){
+            
+        }
+    }
+}
+</script>
+
